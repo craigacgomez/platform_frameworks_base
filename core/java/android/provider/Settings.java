f0dadd266e7532e22f676a4db7567957b1b25f96
Craig Gomez <craigacgomez@gmail.com>
2015-10-31 12:02:22 -0700

    Allow custom user-defined device hostname This allows the user to set a custom device hostname by setting the value of the property net.hostname

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5727,6 +5727,14 @@ public final class Settings {
                 "camera_double_tap_power_gesture_disabled";
 
         /**
+         *  This preference allows the user to set a custom device hostname
+         *
+         * @hide
+         */
+        public static final String DEVICE_HOSTNAME = "device_hostname";
+
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
