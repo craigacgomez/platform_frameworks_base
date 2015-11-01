6b8ad4c20c08aedd5f73d49f99422d27631f4af2
Craig Gomez <craigacgomez@gmail.com>
2015-10-31 21:54:25 -0700

    Disable a lot of debug logging

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -165,7 +165,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
         implements PendingIntent.OnFinished {
     private static final String TAG = "ConnectivityService";
 
-    private static final boolean DBG = true;
+    private static final boolean DBG = false;
     private static final boolean VDBG = false;
 
     private static final boolean LOGD_RULES = false;
@@ -453,7 +453,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
      */
     private class LegacyTypeTracker {
 
-        private static final boolean DBG = true;
+        private static final boolean DBG = false;
         private static final boolean VDBG = false;
         private static final String TAG = "CSLegacyTypeTracker";
 
